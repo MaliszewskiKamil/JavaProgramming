69a7db0de18af5a9406cb8aecca79be442c7fa06
MaliszewskiKamil <kamcio4112@gmail.com>
2021-01-25 12:07:21 +0100

    Played with loops and mathematical equasions

--- src/main/java/Hackerrank/JavaLoopsII.java
+++ src/main/java/Hackerrank/JavaLoopsII.java
@@ -1,2 +1,29 @@
-package Hackerrank;public class JavaLoopsII {
+package Hackerrank;
+
+import java.util.Scanner;
+
+public class JavaLoopsII {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int[] holder = new int[n];
+            int temp = 1;
+            for(int j = 0; j<n; j++){
+                if(j==0){
+                    holder[j] = a+temp*b;
+                    continue;
+                }
+                temp = temp*2;
+                holder[j] = holder[j-1]+temp*b;
+            }
+            for(int j = 0; j<holder.length; j++){
+                System.out.print(holder[j]+" ");
+            }
+        }
+        in.close();
+    }
 }
